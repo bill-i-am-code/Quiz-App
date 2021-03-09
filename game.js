a182a41b5a38ac327191258ddc00003ed4972cd4
bill-i-am-code <69911640+bill-i-am-code@users.noreply.github.com>
2021-03-09 17:04:30 +0000

    end of lessons. I even added a bit myself to show correct answer

--- game.js
+++ game.js
@@ -2,8 +2,10 @@ const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 const progressText = document.getElementById("progressText");
 const scoreText = document.getElementById("score");
-
 const progressBarFull = document.getElementById("progressBarFull");
+const loader = document.getElementById("loader");
+const game = document.getElementById("game");
+
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -11,42 +13,53 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
-        question: "Inside which HTML element do we put the Javascript?",
-        choice1: "<script>",
-        choice2: "<javascript>",
-        choice3: "<js>",
-        choice4: "<scripting>",
-        answer: 1
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called xxx.js?",
-        choice1: "<script href='xxx.js'>",
-        choice2: "<script name='xxx.js'>",
-        choice3: "<script src='xxx.js'>",
-        choice4: "<script file='xxx.js'>",
-        answer: 3
-    },
-    {
-        question: "How do you write 'Hello World' in an alert box",
-        choice1: "msgBox('Hello World')",
-        choice2: "alertBox('Hello World')",
-        choice3: "msg('Hello World')",
-        choice4: "alert('Hello World')",
-        answer: 4
-    }
-];
+let questions = [];
+
+fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple")
+    .then(res => {
+        return res.json();
+    })
+    .then(loadedQuestions => {
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map(loadedQuestion => {
+            const formattedQuestion = {
+                question: loadedQuestion.question
+            };
+            const answerChoices = [...loadedQuestion.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1;
+            answerChoices.splice(
+                formattedQuestion.answer - 1,
+                0,
+                loadedQuestion.correct_answer
+            );
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index + 1)] = choice;
+
+
+            });
+            return formattedQuestion;
+        });
+
+        startGame();
+
+    })
+    .catch(err => {
+        console.error(err);
+    });
+
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 5;
 
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    game.classList.remove("hidden");
+    loader.classList.add("hidden")
 
 };
 
@@ -65,11 +78,11 @@ getNewQuestion = () => {
 
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+    question.innerHTML = currentQuestion.question;
 
     choices.forEach(choice => {
         const number = choice.dataset['number'];
-        choice.innerText = currentQuestion["choice" + number];
+        choice.innerHTML = currentQuestion["choice" + number];
     });
 
     availableQuestions.splice(questionIndex, 1);
@@ -84,7 +97,7 @@ choices.forEach(choice => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset["number"];
-
+        const correctAnswer = choices[currentQuestion.answer - 1];
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
 
         if (classToApply == 'correct') {
@@ -92,10 +105,13 @@ choices.forEach(choice => {
         }
 
 
-        console.log(classToApply)
+
         selectedChoice.parentElement.classList.add(classToApply);
+        correctAnswer.parentElement.classList.add("correct");
+
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply);
+            correctAnswer.parentElement.classList.remove("correct");
             getNewQuestion();
         }, 1000)
 
@@ -110,4 +126,5 @@ incrementScore = num => {
     scoreText.innerText = score;
 }
 
-startGame();
+
+
